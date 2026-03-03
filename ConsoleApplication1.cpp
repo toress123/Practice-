5cbfe6bac2deac4cad996c881f5e9e1141896286
toress123 <121028618+toress123@users.noreply.github.com>
2026-03-04 00:34:43 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//вар1 задание 1 номер 2
+
+int find_max(int arr[], int n) {
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+int main() {
+    int n;
+
+    printf("Vvedite kolichestvo chisel: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("kolichestvo dolzhno bit bolshe 0\n");
+        return 1;
+    }
+
+    int* numbers = (int*)malloc(n * sizeof(int));
+
+    //if (numbers == NULL) {
+    //    printf("Ошибка выделения памяти\n");
+    //    return 1;
+    //}
+
+    printf("Vvedite %d celih chisel:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int maximum = find_max(numbers, n);
+    printf("Maximum = %d\n", maximum);
+
+    free(numbers);
+
+    return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+//вар1 задание 1 номер 3
+
+void printBinary(unsigned short x) {
+    for (int i = 15; i >= 0; i--) {
+        unsigned short mask = 1 << i;
+        printf("%d", (x & mask) ? 1 : 0);
+    }
+}
+
+int main() {
+    unsigned short x;
+
+    printf("Vvedite bezznakovoe kratkoe celoe chislo: ");
+    scanf("%hu", &x);
+
+    printf("Binary predstavlenie chisla:\n");
+    printBinary(x);
+
+    return 0;
+}
